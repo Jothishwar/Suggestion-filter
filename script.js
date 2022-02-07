89c086204f19a93d2465a0dee2ab91e0bc90531a
jothishwar <jothishwarvvr@gmail.com>
2022-02-07 15:54:05 +0530

    added code to display

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function find(word,cities) {
 		return place.city.match(regex) || place.state.match(regex);
 	});
 }
+
+function display() {
+	const arr=find(this.value,cities);
+	const html=arr.map(data=>{
+		const regex=new RegExp(this.value,'gi');
+		const city=data.city.replace(regex,`<span class="hl">${this.value}</span>`);
+		const state=data.state.replace(regex,`<span class="hl">${this.value}</span>`);
+		return `
+			<li>
+			    <span class="name">${city}, ${state}</span>
+			    <span class="population">${data.population}</span>
+			</li>
+		`;
+	}).join(' ');
+	suggestions.innerHTML=html;
+}
+const search=document.querySelector('.search');
+const suggestions=document.querySelector('.suggestions');
+
+search.addEventListener('change',display);
+search.addEventListener('keyup',display);
